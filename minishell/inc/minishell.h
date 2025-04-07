313094477a453705de9b596fd6426914d56f80f8
goncalotr <goncalotribeiro@gmail.com>
2025-04-07 16:27:17 +0100

    refactor(main loop): debug messages with colors

--- minishell/inc/minishell.h
+++ minishell/inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: goteixei <goteixei@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 16:47:25 by goteixei          #+#    #+#             */
-/*   Updated: 2025/04/07 14:40:55 by goteixei         ###   ########.fr       */
+/*   Updated: 2025/04/07 16:20:05 by goteixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,25 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-# include "./../lib/libft_repo/libft/libft.h"
+# include <string.h>
+
+// readline, add_history
+# include <readline/readline.h>
+# include <readline/history.h>
+
+# include "../lib/libft_repo/libft/libft.h"
 
 /**************************************************************************
  * SECTION: Macros
  **************************************************************************/
 
+// Colors
+# define RESET		"\033[0m"
+# define RED		"\033[31m"
+# define GREEN		"\033[32m"
+# define YELLOW		"\033[33m"
+# define BLUE		"\033[34m"
+# define WHITE		"\033[37m"
 
 /**************************************************************************
  * SECTION: Structs
--- minishell/src/main.c
+++ minishell/src/main.c
@@ -6,38 +6,75 @@
 /*   By: goteixei <goteixei@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/06 17:22:18 by goteixei          #+#    #+#             */
-/*   Updated: 2025/04/07 14:38:46 by goteixei         ###   ########.fr       */
+/*   Updated: 2025/04/07 16:26:06 by goteixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./../inc/minishell.h"
+#include "../inc/minishell.h"
 
 // Definition of the global variable
 int	g_last_exit_status = 0;
 
+/**
+ * @brief The main interactive loop of the minishell.
+ *
+ * Reads input using readline, adds it to history, checks for "exit",
+ * and prepares for parsing/execution (currently just prints input).
+ * Handles EOF (Ctrl+D).
+ * 
+ * 1. Read input with prompt
+ * 2. Handle EOF (CTRL+D)
+ * 3. Handle empty input (Enter)
+ * 4. Add non-empty input to history
+ * 5. Check for exit command
+ * 6. Placeholder for parsing
+ * 7. Free input line
+ */
 void	ms_core_loop(void)
 {
-	printf("ms_core_loop");
+	char	*input_line;
+
+	while (1)
+	{
+		input_line = readline(BLUE "minishell> " RESET);
+		if (input_line == NULL)
+		{
+			printf("exit\n");
+			break;
+		}
+		if (input_line[0] == '\0')
+		{
+			free(input_line);
+			continue;
+		}
+		add_history(input_line);
+		if(strcmp(input_line, "exit")==0)
+		{
+			free(input_line);
+			break;
+		}
+		ft_printf(YELLOW "DEBUG Received command: <%s>\n" RESET, input_line);
+		free(input_line);
+		input_line = NULL;
+	}
 }
 
 int	main(int argc, char **argv, char **envp)
 {
 	(void)argc;
 	(void)argv;
+	(void)envp;
 
-	int test1 = argc;
-	char **test2 = argv;
-	char **test3 = envp;
+	// TODO: Initialize environment variables list from envp
+	// TODO: Initialize signal handlers (SIGINT, SIGQUIT)
 
-	(void)test1;
-	(void)test2;
-	(void)test3;
-
-	printf("Welcome to Minishell!\n");
+	printf(GREEN "DEBUG Welcome to Minishell!\n---\n" RESET "\n");
 
 	ms_core_loop();
 
-	printf("Exiting Minishell. Final status: %d\n", g_last_exit_status);
+	//TODO Cleanup
 
-	return (0);
+	printf(RED "\n---\nDEBUG Exiting Minishell. Final status: %d" RESET "\n", g_last_exit_status);
+
+	return (g_last_exit_status);
 }

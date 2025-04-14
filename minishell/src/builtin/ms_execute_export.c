a45c898d3e00c6731d232f6b1f54e9ae532e2057
goncalotr <goncalotribeiro@gmail.com>
2025-04-14 18:50:19 +0100

    feat(builtins export): add name extraction

--- minishell/src/builtin/ms_execute_export.c
+++ minishell/src/builtin/ms_execute_export.c
@@ -6,7 +6,7 @@
 /*   By: goteixei <goteixei@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 01:33:50 by goteixei          #+#    #+#             */
-/*   Updated: 2025/04/09 02:10:06 by goteixei         ###   ########.fr       */
+/*   Updated: 2025/04/14 18:48:05 by goteixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,76 @@
 
 //todo needs linkedlist?
 
+//todo repeat function, merge them and send it to utils
+static int	ms_is_valid_identifier(const char *str)
+{
+	int	i;
+
+	if (!str || str[0] == '\0' || (!ft_isalpha(str[0]) && str[0] != '_'))
+		return (0);
+	i = 1;
+	while (str[i])
+	{
+		if (!ft_isalnum(str[i]) && str[i] != '_')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+//todo find_env_var_index (reuse or copy from unset)
+static int find_env_var_index(const char *name, size_t name_len, char **env_list)
+{
+	int i = 0;
+	if (!name || !env_list)
+		return (-1);
+
+	while (env_list[i])
+	{
+		// Check if the entry starts with "name="
+		if (ft_strncmp(env_list[i], name, name_len) == 0
+			&& env_list[i][name_len] == '=')
+		{
+			return (i); // Found it
+		}
+		i++;
+	}
+	return (-1); // Not found
+}
+
+// Prints the "not a valid identifier" error message for export.
+static void print_export_invalid_identifier_err(const char *arg)
+{
+	ft_putstr_fd("minishell: export: `", STDERR_FILENO);
+	ft_putstr_fd((char *)arg, STDERR_FILENO); // Cast needed if arg is const
+	ft_putstr_fd("': not a valid identifier\n", STDERR_FILENO);
+}
+
+// Extracts the variable name part from an export argument (e.g., "VAR=" -> "VAR").
+// Returns an allocated string for the name, or NULL on allocation failure or if arg is NULL.
+// Also sets name_len via the pointer.
+static char *extract_var_name(const char *arg, size_t *len_name)
+{
+	char *equal_sign;
+	char *name;
+
+	if (!arg) return (NULL);
+
+	equal_sign = ft_strchr(arg, '=');
+	if (equal_sign)
+		*len_name = equal_sign - arg;
+	else
+		*len_name = ft_strlen(arg);
+	name = (char *)malloc(sizeof(char) * (*len_name + 1));
+	if (!name)
+	{
+		perror("minishell: export: malloc error extracting name");
+		return (NULL);
+	}
+	ft_strlcpy(name, arg, *len_name + 1);
+	name[*len_name] = '\0';
+	return (name);
+}
 
 /**
  * @brief Executes the export builtin command.

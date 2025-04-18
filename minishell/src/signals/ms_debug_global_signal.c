e663cd955e84cfd0542ea352164bb26fd8486b31
goncalotr <goncalotribeiro@gmail.com>
2025-04-18 18:36:47 +0100

    style(header): header norminette

--- minishell/src/signals/ms_debug_global_signal.c
+++ minishell/src/signals/ms_debug_global_signal.c
@@ -6,7 +6,7 @@
 /*   By: goteixei <goteixei@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 13:18:29 by goteixei          #+#    #+#             */
-/*   Updated: 2025/04/16 13:33:53 by goteixei         ###   ########.fr       */
+/*   Updated: 2025/04/18 18:23:07 by goteixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 extern unsigned char	g_signal;
 
-void	ms_debug_print_gsig() {
+void	ms_debug_print_gsig(void) {
 
 	ft_printf(YELLOW "DEBUG g_signal = %d\n" RESET, (unsigned int)g_signal);
 }

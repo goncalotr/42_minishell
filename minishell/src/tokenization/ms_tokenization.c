e524df7936edb3fc7f054ffc2eeb1298ef7b0b80
Joao Pedro Freire Vieira <jpedro-f@c2r2s5.42porto.com>
2025-06-25 17:39:59 +0100

    fix: bug in handle quotes

--- minishell/src/tokenization/ms_tokenization.c
+++ minishell/src/tokenization/ms_tokenization.c
@@ -6,7 +6,7 @@
 /*   By: jpedro-f <jpedro-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 14:57:22 by jpedro-f          #+#    #+#             */
-/*   Updated: 2025/06/25 16:32:23 by jpedro-f         ###   ########.fr       */
+/*   Updated: 2025/06/25 17:16:09 by jpedro-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -196,28 +196,9 @@ t_token	*ms_tokenization(t_minishell *data, char *input)
 	tokens = NULL;
 	tokens = ms_start_tokenization(input, tokens);
 	tokens = ms_join_cmd(tokens);
+	ms_print_tokens(tokens);
 	tokens = ms_expansion_check(tokens);
-	//ms_debug_print_tokens(tokens, "1. ms_expansion_check");
-
 	tokens = ms_expand_variables(data, tokens);
-	//ms_debug_print_tokens(tokens, "1. ms_expand_variables");
-
 	tokens = ms_handle_quotes(tokens);
-	// After expansion, we now split CMD tokens into `args`
-	/*
-	t_token *temp = tokens;
-	while (temp)
-	{
-		if (temp->type == TOKEN_CMD && temp->value)
-		{
-			temp->args = ft_split(temp->value, ' '); // space delimiter
-			free(temp->value);
-			temp->value = NULL;
-		}
-		temp = temp->next;
-	}
-	*/
-	//ms_debug_print_tokens(tokens, "3. ms_handle_quotes");
-
 	return (tokens);
 }

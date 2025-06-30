4fdf2a4954a8a40ce9975b6d308cce16ff77f959
goncalotr <goncalotribeiro@gmail.com>
2025-06-30 13:00:21 +0100

    style(norminette): heredoc signals

--- minishell/src/signals/ms_signals_heredoc.c
+++ minishell/src/signals/ms_signals_heredoc.c
@@ -6,13 +6,13 @@
 /*   By: goteixei <goteixei@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/27 10:50:27 by goteixei          #+#    #+#             */
-/*   Updated: 2025/06/30 11:43:56 by goteixei         ###   ########.fr       */
+/*   Updated: 2025/06/30 12:59:37 by goteixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./../../inc/minishell.h"
 
-static void ms_handle_sigint_heredoc(int signum)
+static void	ms_handle_sigint_heredoc(int signum)
 {
 	(void)signum;
 	g_signal = SIGINT;
@@ -20,9 +20,9 @@ static void ms_handle_sigint_heredoc(int signum)
 	close(STDIN_FILENO);
 }
 
-void ms_signal_handlers_set_heredoc(void)
+void	ms_signal_handlers_set_heredoc(void)
 {
-	struct sigaction sa;
+	struct sigaction	sa;
 
 	ft_bzero(&sa, sizeof(sa));
 	sa.sa_handler = ms_handle_sigint_heredoc;
